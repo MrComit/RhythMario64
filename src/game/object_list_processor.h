10eb04cd727da9470b710562b06a2e1b4c2dd95d
MrComit <yozemight@gmail.com>
2021-06-08 17:03:10 -0400

    added another 160 to the object pool capacity because we really can afford it

--- src/game/object_list_processor.h
+++ src/game/object_list_processor.h
@@ -24,7 +24,7 @@
 /**
  * The maximum number of objects that can be loaded at once.
  */
-#define OBJECT_POOL_CAPACITY 340
+#define OBJECT_POOL_CAPACITY 500 // vanilla is 240, i had set it to 340 earlier
 
 /**
  * Every object is categorized into an object list, which controls the order
