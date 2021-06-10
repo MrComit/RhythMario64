bb2d2a6deae7461e860d5742d23d6b8d8df69176
MrComit <yozemight@gmail.com>
2021-06-10 18:46:15 -0400

    added wiseguy's test level script thing

--- levels/entry.c
+++ levels/entry.c
@@ -7,11 +7,19 @@
 
 #include "make_const_nonconst.h"
 
+//#define TEST_STAGE LEVEL_BOB
+
+extern const LevelScript level_main_scripts_entry[];
 const LevelScript level_script_entry[] = {
     INIT_LEVEL(),
     SLEEP(/*frames*/ 2),
     BLACKOUT(/*active*/ FALSE),
-    SET_REG(/*value*/ 0),
+    #ifdef TEST_STAGE
+    SET_REG(TEST_STAGE),
+    EXECUTE(/*seg*/ 0x15, _scriptsSegmentRomStart, _scriptsSegmentRomEnd, level_main_scripts_entry),
+    #else
+    SET_REG(0),
     EXECUTE(/*seg*/ 0x14, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_intro_splash_screen),
+    #endif
     JUMP(/*target*/ level_script_entry),
 };
