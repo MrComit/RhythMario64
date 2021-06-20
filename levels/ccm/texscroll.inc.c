190231ac3cbead9658ff12d87febdf09eeb723de
MrComit <yozemight@gmail.com>
2021-06-19 22:00:58 -0400

    flowing lava

--- levels/ccm/texscroll.inc.c
+++ levels/ccm/texscroll.inc.c
@@ -1,3 +1,113 @@
+void scroll_ccm_dl_falling_lava_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_falling_lava_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.4000000059604645 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_ccm_dl_falling_lava_001_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_falling_lava_001_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.4000000059604645 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_ccm_dl_falling_lava_002_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_falling_lava_002_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.4000000059604645 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_ccm_dl_falling_lava_003_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_falling_lava_003_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.4000000059604645 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_ccm_dl_falling_lava_004_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 6;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_falling_lava_004_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.4000000059604645 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
 void scroll_ccm_dl_Lava_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 37;
@@ -21,5 +131,10 @@ void scroll_ccm_dl_Lava_mesh_layer_1_vtx_0() {
 
 }
 void scroll_ccm() {
+	scroll_ccm_dl_falling_lava_mesh_layer_5_vtx_0();
+	scroll_ccm_dl_falling_lava_001_mesh_layer_5_vtx_0();
+	scroll_ccm_dl_falling_lava_002_mesh_layer_5_vtx_0();
+	scroll_ccm_dl_falling_lava_003_mesh_layer_5_vtx_0();
+	scroll_ccm_dl_falling_lava_004_mesh_layer_5_vtx_0();
 	scroll_ccm_dl_Lava_mesh_layer_1_vtx_0();
 }
