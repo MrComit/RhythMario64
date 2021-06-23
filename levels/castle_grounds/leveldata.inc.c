42450bdd5986a3779dbcea4a675c4f0d5ac7c6fa
MrComit <yozemight@gmail.com>
2021-06-23 18:47:52 -0400

    hub rooms

--- levels/castle_grounds/leveldata.inc.c
+++ levels/castle_grounds/leveldata.inc.c
@@ -1,4 +1,5 @@
 #include "levels/castle_grounds/area_1/collision.inc.c"
+#include "levels/castle_grounds/area_1/room.inc.c"
 #include "levels/castle_grounds/area_1/macro.inc.c"
 #include "levels/castle_grounds/area_1/spline.inc.c"
 #include "levels/castle_grounds/model.inc.c"
